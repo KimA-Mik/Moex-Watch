dd8d3ae1a89d988d8d4d7e8b876147405526b650
KimA-Mik <ananim38@gmail.com>
2023-05-09 14:40:44 +0300

    Added resource provider Added dynamic colors in seclist

--- app/src/main/java/ru/kima/moex/MainActivity.kt
+++ app/src/main/java/ru/kima/moex/MainActivity.kt
@@ -1,7 +1,6 @@
 package ru.kima.moex
 
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import ru.kima.moex.databinding.ActivityMainBinding
 
@@ -9,7 +8,6 @@ import ru.kima.moex.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val mainViewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/ru/kima/moex/views/seclist/SeclistaAdapter.kt
+++ app/src/main/java/ru/kima/moex/views/seclist/SeclistaAdapter.kt
@@ -2,7 +2,10 @@ package ru.kima.moex.views.seclist
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.annotation.ColorInt
 import androidx.recyclerview.widget.RecyclerView
+import ru.kima.moex.R
+import ru.kima.moex.ResourceProvider
 import ru.kima.moex.databinding.ListItemSecurityBinding
 import ru.kima.moex.model.Security
 
@@ -21,6 +24,18 @@ class SeclistaAdapter(
 ) : RecyclerView.Adapter<SecurityHolder>() {
 
     var securities: List<Security> = emptyList()
+    private val resourceProvider = ResourceProvider.getInstance()
+    @ColorInt
+    private var increasing_green = 0
+    @ColorInt
+    private var decreasing_red = 0
+    @ColorInt
+    private var neutral = 0
+
+    init {
+        updateColors()
+    }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SecurityHolder {
         val inflater = LayoutInflater.from(parent.context)
@@ -29,7 +44,6 @@ class SeclistaAdapter(
     }
 
     override fun getItemCount() = securities.size
-
 
     override fun onBindViewHolder(holder: SecurityHolder, position: Int) {
         val security = securities[position]
@@ -41,13 +55,25 @@ class SeclistaAdapter(
             val price = if (!security.WAPRICE.isNaN()) "${String.format("%.2f", security.WAPRICE)}₽"
             else "Empty"
             if (security.LASTCHANGE != 0.0) {
-                binding.wapriceTextView.text = "(${security.LASTCHANGEPRCNT}) $price"
+                binding.wapriceTextView.text = "(${security.LASTCHANGEPRCNT}%) $price"
+                if (security.LASTCHANGE > 0.0) {
+                    binding.wapriceTextView.setTextColor(increasing_green)
+                } else if (security.LASTCHANGE < 0.0) {
+                    binding.wapriceTextView.setTextColor(decreasing_red)
+                }
             } else {
                 binding.wapriceTextView.text = price
+                binding.wapriceTextView.setTextColor(neutral)
             }
             binding.root.setOnClickListener {
                 listener.onSecurityDetail(security)
             }
         }
+    }
+
+    private fun updateColors() {
+        increasing_green = resourceProvider.getColor(R.color.increasing_green)
+        decreasing_red = resourceProvider.getColor(R.color.decreasing_red)
+        neutral = resourceProvider.getColor(R.color.contrast)
     }
 }
